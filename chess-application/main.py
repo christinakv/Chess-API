0fafb7c65e73849e59b3f1db45574d1bfb54b3c4
christinakv <christina.kim1233@gmail.com>
2024-12-30 21:58:08 +0400

    added lifespan

--- chess-application/main.py
+++ chess-application/main.py
@@ -1,17 +1,32 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
+from core.models.db_helper import db_helper
+
+from core.models.base import Base
 
 from api import router as api_router
 
-app = FastAPI()
-app.include_router(
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await db_helper.dispose()
+
+main_app = FastAPI(
+    lifespan=lifespan
+)
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix,)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",
+    uvicorn.run(
+            "main:main_app",
                 host = settings.run.host,
                 port = settings.run.port,
                 reload=True)
