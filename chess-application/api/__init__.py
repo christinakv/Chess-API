b974648522a1f9bda2aeb7ecd4c443c7d9719daa
christinakv <christina.kim1233@gmail.com>
2025-01-02 00:27:01 +0400

    update

--- chess-application/api/__init__.py
+++ chess-application/api/__init__.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from .chess import router as chess_router
+from .queries import router as query_router
 
 router = APIRouter()
-
+router.include_router(chess_router, prefix="/chess", tags=["Chess"])
+router.include_router(query_router, prefix="/queries", tags=["Queries"])
