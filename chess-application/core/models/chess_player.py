9079df720448f8d36e96024a96a3a57da589db52
christinakv <christina.kim1233@gmail.com>
2025-01-02 00:25:25 +0400

    update chess_player.py

--- chess-application/core/models/chess_player.py
+++ chess-application/core/models/chess_player.py
@@ -1,13 +1,13 @@
+from more_itertools.recipes import unique
 from sqlalchemy.orm import Mapped
 from sqlalchemy.testing.schema import mapped_column
 
 from .base import Base
-from sqlalchemy import Column, Integer, String
 
 class ChessPlayer(Base):
     __tablename__ = 'chess_players'
 
-    chess_player_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    chess_player_id: Mapped[int] = mapped_column(unique=True, primary_key=False)
     first_name: Mapped[str] = mapped_column(nullable=False)
     last_name: Mapped[str] = mapped_column(nullable=False)
     country: Mapped[str] = mapped_column(nullable=False)
