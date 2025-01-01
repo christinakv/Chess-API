136d657542a4d7d5f989ab31fb5b9316862f2461
christinakv <christina.kim1233@gmail.com>
2025-01-02 00:25:45 +0400

    update participant.py

--- chess-application/core/models/participant.py
+++ chess-application/core/models/participant.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Mapped
 from sqlalchemy.testing.schema import mapped_column
+from sqlalchemy import ForeignKey
 
 from .base import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
 
 class Participant(Base):
     __tablename__ = 'participants'
 
-    participant_id: Mapped[int] = mapped_column(unique=True, primary_key=True, autoincrement=True)
+    participant_id: Mapped[int] = mapped_column(unique=True, primary_key=False)
     tournament_id: Mapped[int] = mapped_column(ForeignKey('tournaments.tournament_id'))
     chess_player_id: Mapped[int] = mapped_column(ForeignKey('chess_players.chess_player_id'))
     place: Mapped[int] = mapped_column(nullable=False)
