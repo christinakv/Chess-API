6b1e44882923d9d3d166313b1175ab170b3f130d
christinakv <christina.kim1233@gmail.com>
2025-01-02 16:55:06 +0400

    add ui

--- chess-application/main.py
+++ chess-application/main.py
@@ -1,13 +1,10 @@
 from contextlib import asynccontextmanager
-
 import uvicorn
 from fastapi import FastAPI
-
+from fastapi.responses import FileResponse
 from core.config import settings
 from core.models.db_helper import db_helper
-
 from core.models.base import Base
-
 from api import router as api_router
 
 @asynccontextmanager
@@ -18,6 +15,11 @@ async def lifespan(app: FastAPI):
 main_app = FastAPI(
     lifespan=lifespan
 )
+
+@main_app.get("/")
+def root():
+    return FileResponse("../ui/index.html")
+
 main_app.include_router(
     api_router,
     prefix=settings.api.prefix,)
